87a3f93ec8e43fe5dffe9773c8564b9c1c1d04cc
elychr <51218224+elychr@users.noreply.github.com>
2020-12-14 14:00:15 +1100

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,39 +1,35 @@
 let accounts = [
-    { label: 'Accounts Receivable', route: '/docs/accredo/accounts/accounts-receivable', source: require('./pages/accredo_accounts_receivable.md') },
-    { label: 'Accounts Payable', route: '/docs/accredo/accounts/accounts-payable', source: require('./pages/accredo_accounts_payable.md') },
-    { label: 'Cash Book', route: '/docs/accredo/accounts/cashbook', source: require('./pages/accredo_cash_book.md') },
-    { label: 'General Ledger', route: '/docs/accredo/accounts/ledger', source: require('./pages/accredo_general_ledger.md') },
-    { label: 'Goods And Services Tax', route: '/docs/accredo/accounts/gst', source: require('./pages/accredo_gst.md') },
+    { label: 'Accounts Receivable', route: '/accredo/accounts/accounts-receivable', source: require('./pages/accredo_accounts_receivable.md') },
+    { label: 'Accounts Payable', route: '/accredo/accounts/accounts-payable', source: require('./pages/accredo_accounts_payable.md') },
+    { label: 'Cash Book', route: '/accredo/accounts/cashbook', source: require('./pages/accredo_cash_book.md') },
+    { label: 'General Ledger', route: '/accredo/accounts/ledger', source: require('./pages/accredo_general_ledger.md') },
+    { label: 'Goods And Services Tax', route: '/accredo/accounts/gst', source: require('./pages/accredo_gst.md') },
   ]
   
   let accredo = [
-    { label: 'Accounting', route: '/docs/accredo/accounts', children: accounts },
-    { label: 'Inventory Control', route: '/docs/accredo/products', source: require('./pages/accredo_prods.md') },
-    { label: 'Invoicing System', route: '/docs/accredo/sales', source: require('./pages/accredo_sales.md') },
-    { label: 'Purchase Orders', route: '/docs/accredo/purchasing', source: require('./pages/accredo_purchase_orders.md') },
-    { label: 'Job Costing', route: '/docs/accredo/jobs', source: require('./pages/accredo_jobs.md') },
-    { label: 'Company', route: '/docs/accredo/company', source: require('./pages/accredo_company.md') },
+    { label: 'Accounting', route: '/accredo/accounts', children: accounts },
+    { label: 'Inventory Control', route: '/accredo/products', source: require('./pages/accredo_prods.md') },
+    { label: 'Invoicing System', route: '/accredo/sales', source: require('./pages/accredo_sales.md') },
+    { label: 'Purchase Orders', route: '/accredo/purchasing', source: require('./pages/accredo_purchase_orders.md') },
+    { label: 'Job Costing', route: '/accredo/jobs', source: require('./pages/accredo_jobs.md') },
+    { label: 'Company', route: '/accredo/company', source: require('./pages/accredo_company.md') },
   ]
   
   let corefire = [
-    { label: 'Jobs', route: '/docs/corefire/jobs', source: require('./pages/corefire_jobs.md') },
-    { label: 'Solutions', route: '/docs/corefire/solutions', source: require('./pages/corefire_prods.md') },
-    { label: 'Extras', route: '/docs/corefire/extras', source: require('./pages/corefire_extras.md') },
+    { label: 'Jobs', route: '/corefire/jobs', source: require('./pages/corefire_jobs.md') },
+    { label: 'Solutions', route: '/corefire/solutions', source: require('./pages/corefire_prods.md') },
+    { label: 'Extras', route: '/corefire/extras', source: require('./pages/corefire_extras.md') },
   ]
   
-  let commands = [
-    { label: 'ZeroSSL', route: '/docs/commands/zerossl', source: require('./pages/zerossl.md') },
-    { label: 'Random', route: '/docs/commands/random', source: require('./pages/random.md') },      
+  let random = [
+    { label: 'ZeroSSL', route: '/random/zerossl', source: require('./pages/zerossl.md') },
+    { label: 'Commands', route: '/random/cmd', source: require('./pages/random.md') },      
   ]
   
   let routes = [
-    {
-      label: 'Documentation', route: '/docs', children: [
-        { label: 'Accredo', route: '/docs/accredo', children: accredo },
-        { label: 'CoreFire', route: '/docs/corefire', children: corefire },
-        { label: 'Commands', route: '/docs/commands', children: commands }
+        { label: 'Accredo', route: '/accredo', children: accredo },
+        { label: 'CoreFire', route: '/corefire', children: corefire },
+        { label: 'Random', route: '/random', children: commands }
       ]
-    }
-  ]
 
   export default routes
